e245152927e86fccf9d63f9ac244272c5c860001
KevinDasilva19 <kevin14ds@gmail.com>
2021-08-24 21:55:13 -0300

    AgregadoDePHP

---
+++ formulario.php
@@ -0,0 +1,13 @@
+<?php
+$destino = "kevin14ds@gmail.com";
+$nombre = $_POST["nombre"];
+$empresa = $_POST["empresa"];
+$email = $_POST["email"];
+$consulta = $_POST["consulta"];
+
+$contenido = "Nombre: " . $nombre . "\nEmpresa: " . $empresa .  "\nEmail: " . $email . "\nConsulta: " . $consulta ;
+
+mail($destino, "Contacto", $contenido);
+header("location:index.html");
+
+?>
